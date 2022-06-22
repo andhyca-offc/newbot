18967e2e3d2038113127c2b2952e1983610a652e
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-22 22:20:20 +0800

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -8,7 +8,7 @@ const q = {
 	},
 	"message": {
 		"contactMessage": {
-			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285643354073:+62 856-4335-4073\nEND:VCARD"
+			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285822347348:+62 858-2234-7248\nEND:VCARD"
 		}
 	},
 	"participant": "0@s.whatsapp.net"
