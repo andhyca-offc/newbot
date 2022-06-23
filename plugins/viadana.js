e1fb926f84dd4aa143d6c29d90f5a8c42f1a2684
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-23 16:30:22 +0800

    Create viadana.js

---
+++ plugins/viadana.js
@@ -0,0 +1,32 @@
+let handler = async (m, { conn }) => {
+	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
+╠═ Untuk Pembayaran Via DANA
+╠➥ Ke nomer ini
+║➥ 085822347348
+║
+╠═〘 Fitur 〙 ═
+║
+╠➥ *FITUR DOWNLOADER 500++*
+║     *YouTube, Twitter,* 
+║     *Tiktok, Instagram, dll.*
+║    *FITUR STIKER*
+╠➥ *FITUR ADMIN*
+║    *KICK ORANG*
+║    *JADIIN ADMIN*
+╠➥ *FITUR ISLAM*
+║   *QURAN*
+║   *CEK JADWAL SHALAT*
+╠➥ *INTERNET*
+║   *BRAINLY*
+║   *GOOGLE*
+╠➥ *DAN 200 LEBIH*
+║   *FITUR LAINNYA*
+║ 
+╠═ ©2022
+╠═ Scrip original by ArullOfc
+╠═〘 ${namabot} 〙 ═`.trim(), m)
+}
+
+handler.command = /^viadana$/i
+
+module.exports = handler
