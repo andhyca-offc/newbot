04bb59fb0fe32436e3c581896536dc4d0295527f
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-23 13:23:18 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,12 +1,16 @@
-const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
+// RECODE ALYAAXZY
 
-let p = await  hxz.ttdownloader(text)
-const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, 'Nih', m)
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async(m, { conn, usedPrefix, text, command }) => {
+    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSeSCAN1W/`
+    let res = await fetch(global.API('rey', '/api/download/tiktok', { url: text }, 'apikey'))
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let json = await res.json()
+    await conn.sendButtonVid(m.chat, json.result.nowatermark, 'Nih Kak', watermark, 'Thanks', `Thanks`, m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.command = /^tiktok$/i
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
-
+handler.help = ['tiktok']
+handler.limit = true
 module.exports = handler
