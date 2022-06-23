dbff10e76d42841022ebe2801e914cd230e59ab9
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-23 19:16:18 +0800

    Update owner-enable.js

--- plugins/owner-enable.js
+++ plugins/owner-enable.js
@@ -1,9 +1,6 @@
 let handler = async(m, { conn }) => {
 conn.sendContact(m.chat, numberowner, nameowner, m)
 }
-return conn.sendMessage(m.chat, { text: `Hay kak @${await m.sender.split('@')[0]}, itu nomor ownerku jangan dispam yah ^_^`, mentions: [m.sender] }, { quoted: conn.p[id][
-}
-
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 handler.command = /^(owner|creator)$/i
