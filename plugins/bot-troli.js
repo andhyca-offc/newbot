b982b219ee4405bfe72d435d7f3059863a2fdb61
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-23 16:10:11 +0800

    Update bot-troli.js

--- plugins/bot-troli.js
+++ plugins/bot-troli.js
@@ -2,10 +2,10 @@ let fs = require('fs')
 let handler  = async (m, { conn, usedPrefix }) => {
 prep = conn.prepareMessageFromContent(m.chat, { orderMessage: { 
 itemCount: 999999999999, status: 1,
-message: `Follow Github gua Bang\n\nhttps://github.com/BOTCAHX`,
+message: `Join Group Bot\n\nhttps://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKAS`,
 orderTitle: 'B',
 sellerJid: '0@s.whatsapp.net',
-thumbnail: fs.readFileSync('./src/IMG-20211112-WA0021.jpg')
+thumbnail: fs.readFileSync('https://telegra.ph/file/fe53b93567b4263ea0175.jpg')
 }}, {contextInfo: null, quoted: m})
 conn.relayWAMessage(prep)
 }
