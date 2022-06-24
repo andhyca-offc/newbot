9002d068d2abf68e636441b943e2e0cf378d0685
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-25 07:57:17 +0800

    Create yt.js

---
+++ plugins/yt.js
@@ -0,0 +1,19 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
+let botol = global.botwm
+let str = `
+╭═══════════════════════
+║╭──❉ 〔 ⳹YOUTUBE⳹ 〕 ❉────── 
+║│➸ *_YOUTUBE OWNER_*
+║│➸  *https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg*
+╰─────────❉
+▌│█║▌║▌║║▌║▌║█│▌
+ArullBotzMD ⸙ by.ArullOfc
+`.trim()
+conn.sendButton(m.chat, str, `${botol}`, `⋮☰ Menu`, `.menu`, m)
+}
+handler.help = ['yutube']
+handler.tags = ['info']
+handler.command = /^ytzi$|ytrul$/i
+
+module.exports = handler
