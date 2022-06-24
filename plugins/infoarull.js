63e5a4814200bc04d0307dfcd0c668e4fb994b9d
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-24 20:27:19 +0800

    Create infoarull.js

---
+++ plugins/infoarull.js
@@ -0,0 +1,22 @@
+let handler  = async (m, { conn, usedPrefix }) => { 
+conn.reply(m.chat, `
+╭═══════════════════════
+║╭──❉ 〔 INFO OWNER 〕 ❉────── 
+║│➸ ```NAMA``` : Arull
+║│➸ ```UMUR``` : 18thn
+║│➸ ```ASAL``` : KALIMANTAN SELATAN
+║│➸ ```OFFICIAL GRUP``` :
+https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKAS
+║│➸ ```ISTAGRAM``` : http://instagram.com/sahrulwara_____
+║│➸ ```WHATSAPP``` : http://wa.me/6285822347348
+╰────────❉
+`.trim(), m)
+}
+
+handler.help = ['infoarull']
+handler.tags = ['main', 'utama']
+handler.command = /^(infoarull)$/i
+
+handler.exp = 150
+
+module.exports = handler
