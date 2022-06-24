92a2609f2d410770cf8e92deef2bbcccdcda8840
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-25 07:51:23 +0800

    Update downloader-yts.js

--- plugins/downloader-yts.js
+++ plugins/downloader-yts.js
@@ -1,14 +1,14 @@
 let yts = require('yt-search')
-let handler = async (m, { text }) => {
-  if (!text) throw 'Cari apa?'
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `Harap masukkan query yang ingin dicari!\n\nContoh: ${usedPrefix + command} video protes tiananmen square 1989`
   let results = await yts(text)
   let teks = results.all.map(v => {
     switch (v.type) {
       case 'video': return `
 *${v.title}* (${v.url})
-Duration: ${v.timestamp}
-Uploaded ${v.ago}
-${v.views} views
+Durasi: ${v.timestamp}
+Diupload ${v.ago}
+${v.views} Penonton
       `.trim()
       case 'channel': return `
 *${v.name}* (${v.url})
@@ -20,7 +20,7 @@ ${v.videoCount} video
   m.reply(teks)
 }
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
-handler.tags = ['tools', 'internet', 'downloader']
+handler.tags = ['internet']
 handler.command = /^yts(earch)?$/i
 
 module.exports = handler
